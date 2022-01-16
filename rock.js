33f9e0fa74c57afa57a87ebaf1ce4f719431287f
Phil <pdeal06@gmail.com>
2022-01-15 23:17:02 -0600

    Wrote function for computer picks. After tests it seems to work fine

--- rock.js
+++ rock.js
@@ -7,3 +7,27 @@ prints who the winner is in the end.
 
 
 */
+
+/*
+Create function called computerPlay
+Use random method to pick a number between 100
+Create variables cpuRock, cpuPaper, and cpu Scissors
+Make conditional where 1-33 is Rock, 34-66 is Paper, 67-100 is Scissors 
+*/
+
+function computerPlay() {
+     let cpuRPS = Math.floor(Math.random() *100) + 1;
+    if (cpuRPS <= 33){
+        return 'Rock'  
+    }
+    else if (cpuRPS <= 66){
+        return 'Paper'
+
+    }
+    else { 
+        return 'Scissors'
+    }
+
+     
+ 
+}
