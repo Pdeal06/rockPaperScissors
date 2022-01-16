14d40e84a90d81f5c9b0df2aabada0b2e70a2368
Phil <pdeal06@gmail.com>
2022-01-15 22:50:19 -0600

    Wrote general idea in JS note

---
+++ rock.js
@@ -0,0 +1,9 @@
+/*
+What I'm creating is a script that allows someone to play Rock, Paper, Scissors against the computer 
+in the console. The script should take a user prompt of 1 of the choices compare it against the randomized 
+choice by the computer and determine the winner. Eventually, they want a 5 round game that keeps score and 
+prints who the winner is in the end. 
+
+
+
+*/
